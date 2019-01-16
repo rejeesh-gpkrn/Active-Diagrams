bbea22c984e956ed2c1975b5a35f340e041afec3
rejeesh-gpkrn <35416615+rejeesh-gpkrn@users.noreply.github.com>
2019-01-16 23:47:55 +0900

    extension

---
+++ Active Diagrams/source/ContetMenuClickHandler.java
@@ -0,0 +1,27 @@
+package extension;
+
+import com.sun.star.awt.MouseEvent;
+import com.sun.star.awt.XMouseClickHandler;
+import com.sun.star.lang.EventObject;
+
+public class ContetMenuClickHandler implements XMouseClickHandler {
+
+	@Override
+	public void disposing(EventObject arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
