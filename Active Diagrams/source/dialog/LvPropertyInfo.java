6ef567c04d4a721e59b452a5504a8e043d7bb1f2
rejeesh-gpkrn <35416615+rejeesh-gpkrn@users.noreply.github.com>
2019-01-16 23:45:59 +0900

    Add files via upload

---
+++ Active Diagrams/source/dialog/LvPropertyInfo.java
@@ -0,0 +1,21 @@
+package dialog;
+
+public class LvPropertyInfo {
+	
+	private String name;
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+}
