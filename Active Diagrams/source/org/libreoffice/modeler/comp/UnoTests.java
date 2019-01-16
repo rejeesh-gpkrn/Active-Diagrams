dcc156390e335dd973f4447074353721e83222ad
rejeesh-gpkrn <35416615+rejeesh-gpkrn@users.noreply.github.com>
2019-01-16 23:52:53 +0900

    Add files via upload

---
+++ Active Diagrams/source/org/libreoffice/modeler/comp/UnoTests.java
@@ -0,0 +1,12 @@
+package org.libreoffice.modeler.comp.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+import org.libreoffice.modeler.comp.tests.base.UnoSuite;
+import org.libreoffice.modeler.comp.tests.uno.WriterTest;
+
+@RunWith(UnoSuite.class)
+@SuiteClasses({WriterTest.class})
+public class UnoTests {
+
+}
